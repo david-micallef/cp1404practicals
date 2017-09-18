de330558ad573711506dc2d72b4f8131c1578dfd
david-micallef <david.micallef@my.jcu.edu.au>
2017-09-18 14:20:40 +1000

    Add python code for getting user input and displaying guitars

---
+++ prac_07/guitars.py
@@ -0,0 +1,23 @@
+from prac_07.guitar import Guitar
+
+guitars = []
+print("My guitars!")
+guitar_name = "None"
+while guitar_name != "":
+    guitar_name = str(input("Name: "))
+    if guitar_name == "":
+        print("... snip ...")
+        break
+    guitar_year = int(input("Year: "))
+    guitar_cost = float(input("Cost: $"))
+    guitars.append(Guitar(guitar_name, guitar_year, guitar_cost))
+# guitars.append(Guitar("Gibson L-5 CES", 1922, 16035.40))
+# guitars.append(Guitar("Line 6 JTV-59", 2010,1512.9))
+
+
+for i in range(len(guitars)):
+    if guitars[i].is_vintage():
+        vintage = "(vintage)"
+    else:
+        vintage = ""
+    print("Guitar {}: {:>20} ({}), worth ${:10,.2f} {}".format(i + 1, guitars[i].name, guitars[i].year,guitars[i].cost, vintage))
