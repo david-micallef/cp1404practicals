28f191a93883e20d568e1894e6ee225ed835800c
david-micallef <david.micallef@my.jcu.edu.au>
2017-09-25 13:16:26 +1000

    Add Test Taxi python code

---
+++ prac_08/taxi_test.py
@@ -0,0 +1,13 @@
+from prac_08.taxi import Taxi
+
+
+taxi_one = Taxi("Prius 1", 100, 1.23)
+taxi_one.drive(40)
+current_fare = taxi_one.get_fare()
+print(taxi_one)
+print("Current taxi fare is: ${:.2f}".format(current_fare))
+taxi_one.start_fare()
+taxi_one.drive(100)
+current_fare = taxi_one.get_fare()
+print(taxi_one)
+print("Current taxi fare is: ${:.2f}".format(current_fare))
