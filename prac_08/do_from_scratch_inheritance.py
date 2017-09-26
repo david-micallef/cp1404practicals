44a23299657fb1ffca2530728d911addf45a69bd
david-micallef <david.micallef@my.jcu.edu.au>
2017-09-26 16:06:43 +1000

    Add python code for simulator program that uses Taxi and SilverServiceTaxi classes

---
+++ prac_08/do_from_scratch_inheritance.py
@@ -0,0 +1,32 @@
+from prac_08.taxi import Taxi
+from prac_08.silver_service_taxi import SilverServiceTaxi
+
+taxis = [Taxi("Prius", 100), SilverServiceTaxi("Limo", 100, 2), SilverServiceTaxi("Hummer", 200, 4)]
+
+total_bill = 0
+print("Let's drive!")
+print("q)uit, c)hoose taxi, d)rive")
+menu_choice = str(input(">>> ").lower())
+while menu_choice != "q":
+    if menu_choice == "c":
+        print("Taxis available:")
+        for i,taxi in enumerate(taxis):
+            print("{} - {}".format(i,taxi))
+        taxi_choice = int(input("Choose taxi: "))
+        print("Bill to date: ${:.2f}".format(total_bill))
+    elif menu_choice == "d":
+        taxis[taxi_choice].start_fare()
+        distance_to_travel = int(input("Drive how far? "))
+        taxis[taxi_choice].drive(distance_to_travel)
+        current_fare = taxis[taxi_choice].get_fare()
+        total_bill += current_fare
+        print("Your {} trip cost you ${:.2f}".format(taxis[taxi_choice].name, current_fare))
+        print("Bill to date: ${:.2f}".format(total_bill))
+    print("q)uit, c)hoose taxi, d)rive")
+    menu_choice = str(input(">>> ").lower())
+print("Total trip cost: ${:.2f}".format(total_bill))
+print("Taxis are now: ")
+for i, taxi in enumerate(taxis):
+    print("{} - {}".format(i, taxi))
+
+
