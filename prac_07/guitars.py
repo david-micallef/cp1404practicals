67b81dd64f1fe3ed7a60e8711b99d94257d91baf
david-micallef <david.micallef@my.jcu.edu.au>
2017-10-03 13:19:37 +1000

    Made Corrections

--- prac_07/guitars.py
+++ prac_07/guitars.py
@@ -2,12 +2,12 @@ from prac_07.guitar import Guitar
 
 guitars = []
 print("My guitars!")
-guitar_name = str(input("Name: "))
+guitar_name = input("Name: ")
 while guitar_name != "":
     guitar_year = int(input("Year: "))
     guitar_cost = float(input("Cost: $"))
     guitars.append(Guitar(guitar_name, guitar_year, guitar_cost))
-    guitar_name = str(input("Name: "))
+    guitar_name = input("Name: ")
 # guitars.append(Guitar("Gibson L-5 CES", 1922, 16035.40))
 # guitars.append(Guitar("Line 6 JTV-59", 2010,1512.9))
 
