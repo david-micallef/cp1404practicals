7895955632a0a1539e85104a409ca370fa52b1d6
david-micallef <david.micallef@my.jcu.edu.au>
2017-11-14 18:05:34 +1000

    Made Corrections

--- prac_08/silver_service_taxi.py
+++ prac_08/silver_service_taxi.py
@@ -7,7 +7,7 @@ class SilverServiceTaxi(Taxi):
     def __init__(self, name, fuel, fanciness=0):
         super().__init__(name, fuel)
         self.fanciness = fanciness
-        self.price_per_km = self.fanciness * Taxi.price_per_km
+        self.price_per_km *= self.fanciness
 
     def __str__(self):
         return "{}, plus flagfall of ${:.2f}".format(super().__str__(), self.flagfall)
--- prac_08/unreliable_car.py
+++ prac_08/unreliable_car.py
@@ -3,16 +3,14 @@ import random
 
 
 class UnreliableCar(Car):
-    def __init__(self, name="", fuel=0, reliability=random.uniform(0,100)):
+    def __init__(self, name="", fuel=0, reliability=random.uniform(0, 100)):
         super().__init__(name, fuel)
         self.reliability = reliability
 
     def drive(self, distance):
-        chance_to_drive = random.uniform(0,100)
+        chance_to_drive = random.uniform(0, 100)
         if chance_to_drive > self.reliability:
             distance_driven = super().drive(distance)
-            return distance_driven
         else:
             distance_driven = 0
-            return distance_driven
-
+        return distance_driven
